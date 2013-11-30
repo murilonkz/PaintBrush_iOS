f0cb96c32ddba2e0808e38d0512ef1b2d8383f60
Murilo Campaner <murilocampaner@MacBook-Pro-de-Murilo.local>
2013-11-30 14:19:08 -0200

    Inicio Projeto

---
+++ PaintBrush_iOS/PaintBrush_iOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaintBrush_iOS
+//
+//  Created by Murilo Campaner on 30/11/13.
+//  Copyright (c) 2013 Murilo Campaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
