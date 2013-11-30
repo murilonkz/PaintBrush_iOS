ad48ba83762472594428611f89ca685dae0fa5a8
Murilo Campaner <murilocampaner@MacBook-Pro-de-Murilo.local>
2013-11-30 15:03:17 -0200

    …

---
+++ PaintBrush_iOS/PaintBrush_iOS/IndexViewController.h
@@ -0,0 +1,17 @@
+//
+//  IndexViewController.h
+//  PaintBrush_iOS
+//
+//  Created by Murilo Campaner on 30/11/13.
+//  Copyright (c) 2013 Murilo Campaner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IndexViewController : UIViewController
+- (IBAction)getCircle:(id)sender;
+- (IBAction)getPentagono:(id)sender;
+- (IBAction)getRectangle:(id)sender;
+- (IBAction)getText:(id)sender;
+
+@end
---
+++ PaintBrush_iOS/PaintBrush_iOS/IndexViewController.m
@@ -0,0 +1,49 @@
+//
+//  IndexViewController.m
+//  PaintBrush_iOS
+//
+//  Created by Murilo Campaner on 30/11/13.
+//  Copyright (c) 2013 Murilo Campaner. All rights reserved.
+//
+
+#import "IndexViewController.h"
+
+@interface IndexViewController ()
+
+@end
+
+@implementation IndexViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)getCircle:(id)sender {
+}
+
+- (IBAction)getPentagono:(id)sender {
+}
+
+- (IBAction)getRectangle:(id)sender {
+}
+
+- (IBAction)getText:(id)sender {
+}
+@end
